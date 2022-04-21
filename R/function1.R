15f9eea243a5495cf23b333a1f2edac4cda660a4
Ioannis Mavrogiannis <imavrog3@gmail.com>
2022-04-21 17:56:32 +0200

    Initial commit

---
+++ R/function1.R
@@ -0,0 +1,16 @@
+
+
+#' A simply incredible function
+#'
+#' @param x Some description here.
+#'
+#' @return We return the x value back!
+#' @export
+#'
+#' @examples
+#' x <- 'an amazing character vector'
+#' fun(x)
+fun <- function(x){
+    eh <- x
+    print(eh)
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(dsmmR)
+
+test_check("dsmmR")
